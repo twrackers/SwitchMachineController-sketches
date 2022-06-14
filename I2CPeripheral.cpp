53f6888f5db62d3e60ead30f704b11cf18a51dd9
Thomas W Rackers <twrackers@users.noreply.github.com>
2022-06-13 21:29:05 -0400

    Move address arg from constructor to begin().

--- I2CPeripheral.cpp
+++ I2CPeripheral.cpp
@@ -4,18 +4,19 @@
 
 // I2CPeripheral class - Handles device end of I2C link to central processor.
 I2CPeripheral::I2CPeripheral(
-  const byte addr,      // I2C address of this processor
   CALLBACK1 receive,    // address of onReceive callback
   CALLBACK0 request     // address of onRequest callback
-) : m_addr(addr)
+)
 {
   // Set up callbacks for I2C interface.
   Wire.onReceive(receive);
   Wire.onRequest(request);
 }
 
-void I2CPeripheral::begin()
+void I2CPeripheral::begin(
+  const byte addr       // I2C address of this processor
+)
 {
   // Begin I2C interface.
-  Wire.begin(m_addr);
+  Wire.begin(addr);
 }
